960235fcac25d636b2f8b3b00c127964ea9cb8a6
Welingson Expedito dos Santos <welingson12@gmail.com>
2024-09-26 14:54:22 -0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
+import "@/app/ui/global.css";
+import { montserrat } from "@/app/ui/fonts";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="pt-BR">
+      <body className={`${montserrat.className} antialiased`}>{children}</body>
     </html>
   );
 }
